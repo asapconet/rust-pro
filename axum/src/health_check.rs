52ec385eff071e8b325eee35240a1986b7dd26f5
ASAP_A1 <aaronrickymeek@gmail.com>
2026-01-16 21:07:35 +0100

    test: add test to health check Apis

--- axum/src/health_check.rs
+++ axum/src/health_check.rs
@@ -36,7 +36,7 @@ impl IntoResponse for ApiError {
 }
 
 async fn health_check() -> impl IntoResponse {
-    Json(json!({ "status" :"ok","message":"Sever is running",}))
+    Json(json!({ "status": "ok", "message": "Server is running",}))
 }
 
 async fn list_users() -> Result<Json<Value>, ApiError> {
@@ -68,4 +68,52 @@ pub async fn main() {
     axum::serve(listener, app)
         .await
         .expect("failed to start server")
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use serde_json::Value;
+    use tower::ServiceExt;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_health_cheak() {
+        let app = create_app();
+
+        let request = Request::builder()
+            .uri("/health")
+            .body(Body::empty())
+            .unwrap();
+
+        let response = app.oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap();
+        let json: Value = serde_json::from_slice(&body.to_bytes()).unwrap();
+
+        assert_eq!(json["status"], "ok");
+        assert_eq!(json["message"], "Server is running");
+    }
+
+    #[tokio::test]
+    async fn test_api_error_into_response() {
+        let test_cases = vec![
+            (ApiError::NotFound, StatusCode::NOT_FOUND),
+            (
+                ApiError::InvalidInput("Wrong input".to_string()),
+                StatusCode::BAD_REQUEST,
+            ),
+            (ApiError::InternalError, StatusCode::INTERNAL_SERVER_ERROR),
+        ];
+        for (error, expected_status) in test_cases {
+            let response = error.into_response();
+            assert_eq!(response.status(), expected_status);
+        }
+    }
 }
